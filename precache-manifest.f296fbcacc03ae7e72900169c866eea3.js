e315d67eb69468adc636e57fcd4a430b5cf6a18d
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2020-05-14 21:27:57 +0530

    Updates

---
+++ precache-manifest.f296fbcacc03ae7e72900169c866eea3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "337d999bf350eac5f655a408c961bda1",
+    "url": "/amit-ghosh.com/ec-front/index.html"
+  },
+  {
+    "revision": "5f1c5fd9eda094337d9c",
+    "url": "/amit-ghosh.com/ec-front/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "626f2f7167ff3464e83b",
+    "url": "/amit-ghosh.com/ec-front/static/js/2.527483b1.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/amit-ghosh.com/ec-front/static/js/2.527483b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f1c5fd9eda094337d9c",
+    "url": "/amit-ghosh.com/ec-front/static/js/main.74d041be.chunk.js"
+  },
+  {
+    "revision": "e7236274753a7dd18a82",
+    "url": "/amit-ghosh.com/ec-front/static/js/runtime-main.7e2129d9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/amit-ghosh.com/ec-front/static/media/logo.5d5d9eef.svg"
+  }
+]);
