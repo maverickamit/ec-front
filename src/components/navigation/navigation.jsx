e5da707dde70fb201a057e33f63a865473b0ac73
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2020-06-08 21:25:15 +0530

    displaying navbar right side links properly

--- src/components/navigation/navigation.jsx
+++ src/components/navigation/navigation.jsx
@@ -29,7 +29,7 @@ const NavigationBar = ({ userStore }) => {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
+          <ul className="navbar-nav ml-auto">
             <li className="nav-item active">
               <a className="nav-link">
                 <button
@@ -76,7 +76,7 @@ const NavigationBar = ({ userStore }) => {
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
+        <ul className="navbar-nav ml-auto">
           <li className="nav-item active">
             <a className="nav-link">
               <Link to="/">Login</Link>
