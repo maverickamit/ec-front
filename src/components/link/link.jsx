f3f61351c639c6a8b4169055ab7a78470fa60ea9
Amit <29959138+maverickamit@users.noreply.github.com>
2020-12-02 11:39:36 +0530

    feat: implented plaid link integration for reverification of bank account

--- src/components/link/link.jsx
+++ src/components/link/link.jsx
@@ -7,46 +7,70 @@ import fetchUser from "../modules/fetchUser";
 const Link = ({ userStore }) => {
   const onSuccess = useCallback((token, metadata) => {
     // send token to server
-    fetch(prodUrl + "/users/banking/plaidverify", {
-      method: "post",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + userStore.token,
-      },
-      body: JSON.stringify({
-        PUBLIC_TOKEN: token,
-        ACCOUNT_ID: metadata.account_id,
-      }),
-    })
-      .then((response) => {
-        if (response.status === 200) {
-          fetchUser({ userStore });
-          alert("Account successfully linked");
-        } else {
-          alert("Error in linking account");
-        }
+    if (config.token == null) {
+      fetch(prodUrl + "/users/banking/plaidverify", {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + userStore.token,
+        },
+        body: JSON.stringify({
+          PUBLIC_TOKEN: token,
+          ACCOUNT_ID: metadata.account_id,
+        }),
       })
-      .catch((err) => {
-        alert("Error in linking account");
-        console.log(err);
-      });
+        .then((response) => {
+          if (response.status === 200) {
+            fetchUser({ userStore });
+            alert("Account successfully linked");
+          } else {
+            alert("Error in linking account");
+          }
+        })
+        .catch((err) => {
+          alert("Error in linking account");
+          console.log(err);
+        });
 
-    console.log("Public Token: " + token);
-    console.log("Customer-selected account ID: " + metadata.account_id);
+      console.log("Public Token: " + token);
+      console.log("Customer-selected account ID: " + metadata.account_id);
+    } else {
+      console.log("Public Token: " + token);
+      console.log("metadata: " + metadata);
+      console.log("successful update");
+    }
   }, []);
 
-  const config = {
-    clientName: "Your Link name",
-    env: "sandbox",
-    product: ["auth"],
-    publicKey: "c920232687f1eaf83b9790d1f0fdc5",
-    onSuccess,
-    // ...
-  };
-
+  if (userStore.user.linkUpdateToken == "") {
+    var config = {
+      clientName: "Your Link name",
+      env: "sandbox",
+      product: ["auth"],
+      publicKey: "c920232687f1eaf83b9790d1f0fdc5",
+      onSuccess,
+      // ...
+    };
+  } else {
+    var config = {
+      token: userStore.user.linkUpdateToken,
+      onSuccess,
+    };
+  }
   const { open, ready, error } = usePlaidLink(config);
 
   if (userStore.user.bankLinked) {
+    if (userStore.user.linkUpdateToken != "") {
+      return (
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={() => open()}
+          disabled={!ready}
+        >
+          Reverify bank account
+        </button>
+      );
+    }
     return (
       <button type="button" className="btn btn-success">
         Bank Account Connected
