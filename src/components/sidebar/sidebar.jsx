6fdd6cdde9c9505fe06e0a12b79b55a2570a80a6
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2021-04-29 13:06:48 +0530

    fix: minor fix sidebar component

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -6,63 +6,63 @@ import styles from "./sidebar.module.css";
 
 const SideBar = ({ userStore }) => {
   return (
-    <nav class="col-md-2 d-none d-md-block bg-light sidebar">
-      <div class="sidebar-sticky">
-        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase font-weight-bold">
+    <nav className={"col-md-2 d-none d-md-block bg-light " + styles.sidebar}>
+      <div className="sidebar-sticky">
+        <h6 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase font-weight-bold">
           <span>Accout</span>
-          <a class="d-flex align-items-center text-muted" href="#">
+          <a className="d-flex align-items-center text-muted" href="#">
             <span data-feather="plus-circle"></span>
           </a>
         </h6>
-        <ul class="nav flex-column">
-          <li class="nav-groups">
-            <a class="nav-link active" href="#">
+        <ul className="nav flex-column">
+          <li className="nav-groups">
+            <a className="nav-link active" href="#">
               Accout Summary
             </a>
           </li>
         </ul>
-        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase font-weight-bold">
+        <h6 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase font-weight-bold">
           <span>Contributions</span>
-          <a class="d-flex align-items-center text-muted" href="#">
+          <a className="d-flex align-items-center text-muted" href="#">
             <span data-feather="plus-circle"></span>
           </a>
         </h6>
-        <ul class="nav flex-column">
-          <li class="nav-item">
-            <a class="nav-link active" href="#">
+        <ul className="nav flex-column">
+          <li className="nav-item">
+            <a className="nav-link active" href="#">
               <span data-feather="home"></span>
-              Activity <span class="sr-only">(current)</span>
+              Activity <span className="sr-only">(current)</span>
             </a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
+          <li className="nav-item">
+            <a className="nav-link" href="#">
               <span data-feather="file"></span>
               Update Charity
             </a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
+          <li className="nav-item">
+            <a className="nav-link" href="#">
               <span data-feather="shopping-cart"></span>
               Suggest a Charity
             </a>
           </li>
         </ul>
 
-        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase font-weight-bold">
+        <h6 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase font-weight-bold">
           <span>Settings</span>
-          <a class="d-flex align-items-center text-muted" href="#">
+          <a className="d-flex align-items-center text-muted" href="#">
             <span data-feather="plus-circle"></span>
           </a>
         </h6>
-        <ul class="nav flex-column mb-2">
-          <li class="nav-item">
-            <a class="nav-link" href="#">
+        <ul className="nav flex-column mb-2">
+          <li className="nav-item">
+            <a className="nav-link" href="#">
               <span data-feather="file-text"></span>
               Update Profile
             </a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
+          <li className="nav-item">
+            <a className="nav-link" href="#">
               <span data-feather="file-text"></span>
               Link Bank Account
             </a>
