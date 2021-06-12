dc1ec9eec35837dea4dc3129ccc221c304b5a8c9
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2021-06-12 19:12:18 +0530

    feat: update layout, add new elements

--- src/components/profile/linkedAccountInfo/linkedAccountInfo.jsx
+++ src/components/profile/linkedAccountInfo/linkedAccountInfo.jsx
@@ -1,14 +1,24 @@
 import React, { useState } from "react";
 import { observer } from "mobx-react";
 import Link from "../../link/link";
-import "./../profile.css";
-import styles from "./../profile.module.css";
+
 const LinkedAccountInfo = ({ userStore }) => {
   return (
-    <div>
+    <div className="border border-grey px-3">
       {/* Account Linking Status*/}
-      <div className="row mt-5">
-        <div>
+      <div className="row mt-5 ">
+        <div className="col-sm-6 col-md-4  ">
+          <p>Accounts</p>
+          <p>Bank</p>
+          <h3>Bank of America</h3>
+        </div>
+        <div className="col-sm-6 col-md-4 text-center">
+          <p>Account Name</p>
+          <h3>XXXXX-1234</h3>
+        </div>
+        <div className="col-sm-6 col-md-4 text-right">
+          <p>Link a Bank Account</p>
+          <p>Status</p>
           <Link userStore={userStore} />
           <br />
           <br />
