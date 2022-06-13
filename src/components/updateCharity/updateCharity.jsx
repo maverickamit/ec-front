f5788f3455897ac1e1b81b24a3ddb35c23a4dbdf
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2022-06-13 18:37:53 +0530

    feat: implement update current charity form - update current charity from available options in drop down menu - fetch and display current charity

--- src/components/updateCharity/updateCharity.jsx
+++ src/components/updateCharity/updateCharity.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Field, Form, Formik } from "formik";
 import { observer } from "mobx-react";
 import { Redirect } from "react-router-dom";
 import { useMediaQuery } from "react-responsive";
+import axios from "axios";
 import NotificationModal from "../modal/notification";
+import Select from "react-select";
+
+import fetchUser from "../modules/fetchUser";
+import { prodUrl } from "../urls";
 import "./updateCharity.css";
 import styles from "./updateCharity.module.css";
 
@@ -11,13 +17,58 @@ const UpdateCharityPage = ({ userStore }) => {
   const isMobile = useMediaQuery({ query: "(max-width: 500px)" });
   const updateCharityWidth = isTabletOrMobile ? "col-md-9" : "col-md-10";
 
+  const [options, setOptions] = useState([]);
+  const [currentCharity, setCurrentCharity] = useState("");
+
+  //this fetches list of charities to display on dropdown menu
+
+  useEffect(() => {
+    const getValues = async () => {
+      try {
+        const response = await axios.get(prodUrl + "/users/charities");
+        setOptions(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getValues();
+  }, []);
+
+  //this fetches and displays the current charity selected by the user
+
+  useEffect(() => {
+    const getCurrentCharity = async () => {
+      try {
+        const response = await axios.get(prodUrl + "/users/charities");
+        const result = response.data.filter(
+          (item) => item.value === userStore.user.currentCharity.id
+        );
+        setCurrentCharity(result[0].label);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getCurrentCharity();
+  }, [userStore.user.currentCharity.id]);
+
   if (!userStore.loggedIn) {
     return <Redirect to="/" />;
   }
 
+  const SelectField = ({ field, form, options }) => (
+    <Select
+      options={options}
+      name={field.name}
+      value={
+        options ? options.find((option) => option.value === field.value) : ""
+      }
+      onChange={(option) => form.setFieldValue(field.name, option.value)}
+      onBlur={field.onBlur}
+    />
+  );
+
   return (
-    <div className={"col-xs-12 col-sm-12 " + ` ${updateCharityWidth} `}>
-      {" "}
+    <div className={`col-xs-12 col-sm-12 ${updateCharityWidth} `}>
       <NotificationModal userStore={userStore} />
       <div
         className={
@@ -27,36 +78,50 @@ const UpdateCharityPage = ({ userStore }) => {
         <div className="card-body">
           <h3 className="card-title text-center">Update Charity</h3>
           <div className="card-text" />
-          <form
-            className={styles.inputForm}
-            onSubmit={(e) => e.preventDefault()}
+          <Formik
+            initialValues={{
+              charityId: userStore.user.currentCharity.id,
+            }}
+            onSubmit={(values, actions) => {
+              axios
+                .patch(
+                  prodUrl + "/users/me/charity",
+                  { charityId: values.charityId },
+                  {
+                    headers: {
+                      "Content-Type": "application/json",
+                      Authorization: "Bearer " + userStore.token,
+                    },
+                  }
+                )
+                .then(() => {
+                  userStore.setNotification("Current charity updated!");
+                  userStore.setIsNotification(true);
+                });
+            }}
           >
-            <div className="form-group">
-              <label htmlFor="charityList">
-                Select charity from the dropdown menu
-              </label>
-              <select
-                className="form-control"
-                id="currentCharity"
-                name="currentCharity"
-                type="text"
-              >
-                <option selected>Open this select menu</option>
-                <option value="1">Wounded Warriors Project</option>
-              </select>
-            </div>
-            <button
-              type="submit"
-              className="btn btn-primary"
-              onMouseDown={(e) => e.preventDefault()}
-            >
-              Update
-            </button>
-          </form>
+            {(props) => (
+              <Form className={styles.inputForm}>
+                <div className="form-group">
+                  <label htmlFor="charityList">
+                    Select charity from the dropdown menu
+                  </label>
+                  <Field
+                    name={"charityId"}
+                    component={SelectField}
+                    options={options}
+                  />
+                </div>
+                <button type="submit" className="btn btn-primary">
+                  Submit
+                </button>
+              </Form>
+            )}
+          </Formik>
           <br></br>
           <div>
             <span className="currentCharity">Current charity: </span>
-            {`Wounded Warriors Project`}
+            {currentCharity}
           </div>
         </div>
       </div>
